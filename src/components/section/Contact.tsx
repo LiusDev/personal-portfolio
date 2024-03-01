282567d43bc8655e828bff9e0ffa900f7dddf4aa
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-03-01 20:49:48 +0700

    fix: contact

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { Title } from "..";
+import React from "react"
+import { Title } from ".."
+import { cn } from "@/utils"
 
-const Contact = ({ id, className }) => {
+const Contact = ({
+    id,
+    className = "",
+}: {
+    id: string
+    className?: string
+}) => {
     return (
-        <section id={id} className={`w-full md:mt-0 px-6 md:px-8 ${className}`}>
+        <section
+            id={id}
+            className={cn(`w-full md:mt-0 px-6 md:px-8 ${className}`)}
+        >
             <div className="max-w-screen-lg m-auto">
                 <Title>Contact</Title>
             </div>
         </section>
-    );
-};
+    )
+}
 
-export default Contact;
+export default Contact
